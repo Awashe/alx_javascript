4037309db6163c33c8606028316b12d7a074f3b2
Awash Hedato <awashedato@gmail.com>
2023-09-17 17:45:22 +0300

    update console

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -3,24 +3,14 @@ const myObject = {
     value: 12
   };
   console.log(myObject);
-  myObject.incr = function (){
+  
+  myObject.incr = function() {
     this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
-  // Remove the name property of the incr function
-  Object.defineProperty(myObject.incr, 'name', { 
-    value: '',
-    writable: false
-  });
-  
-  // Use the incr function without additional console.log statements
-  for(let i = 0; i < 2; i++){
-    myObject.incr();
   }
+  
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
   console.log(myObject);
