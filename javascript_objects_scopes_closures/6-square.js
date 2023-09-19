984df00b1116993cd492688e213a180530724de3
Awash Hedato <awashedato@gmail.com>
2023-09-19 12:47:46 +0300

    bulk update

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
-const SquareBase = require('./5-square');
+const SquareP = require('./5-square');
 
-class Square extends SquareBase {
-  charPrint(c) {
-    if (!c) {
+class Square extends SquareP {
+  charPrint (c) {
+    if (typeof c === undefined) {
       c = 'X';
     }
-    
-    let row = '';
-    
-    // Create a row of c characters with length of the square's size
-    for (let i = 0; i < this.size; i++) {
-      row += c;
-    }
-    
-    // Print the row for the size of the square
-    for (let i = 0; i < this.size; i++) {
-      console.log(row);
+    for (let i = 0; i < this.height; i++) {
+      let k = '';
+      for (let j = 0; j < this.width; j++) {
+        k += c;
+      }
+      console.log(k);
     }
   }
 }
