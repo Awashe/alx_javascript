abe105f814ae93c8300a54eb9b1393d785f0ba0f
Awash Hedato <awashedato@gmail.com>
2023-09-19 12:44:33 +0300

    6. Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+    charPrint(c){
+        if (!c) {
+            c = 'X';
+        }
+        let row = ' ';
+        // Create a row of c characters with length of the square's size
+        for (let i = 0; i < this.size; i++) {
+            row += c;
+          }
+          
+          // Print the row for the size of the square
+          for (let i = 0; i < this.size; i++) {
+            console.log(row);
+          }
+    }
+}
+module.exports = Square;
