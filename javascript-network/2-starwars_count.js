19f96e172ac6f5feb40f9ab8564f7fd8b84b432d
Awash Hedato <awashedato@gmail.com>
2023-09-23 11:41:47 +0300

    2. Star wars Wedge Antilles

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, function(error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const wedgeAntillesFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+    console.log(`Wedge Antilles appears in ${wedgeAntillesFilms.length} films.`);
+  }
+});
