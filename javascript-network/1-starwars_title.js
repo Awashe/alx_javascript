385eef7eed50fdd7a968a5201756156ff9667b84
Awash Hedato <awashedato@gmail.com>
2023-09-23 11:39:06 +0300

    1. Star wars movie title

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,11 @@
+const request = require('request');
+const movieId = process.argv[2];
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}`, function(error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
